3306d753fe1b7a917839495cff6d39d0ffa3e84d
Rafael Dalvas <rafaeldalvas@gmail.com>
2020-10-21 21:59:45 -0300

    iniciando ct01 criar_atividade

--- pages/page_criar_atividade.py
+++ pages/page_criar_atividade.py
@@ -25,13 +25,14 @@ class criarAtividade(PageElement):
     palestrante = (By.CSS_SELECTOR, 'tr[id$="comp-228"] [class="z-combo-item-text"]') #Alberto Duque Portugal
 
     #DATAS DA ATIVIDADE
-    botao_definir = (By.CSS_SELECTOR, 'table[id$="comp-159!box"] [class$="button-cm"]')
-    sala = (By.ID, 'zk-comp-1110')
-    Data = (By.ID, 'zk-comp-1113!real')
-    hora_inicio = (By.ID, 'zk-comp-1116')
-    hora_fim = (By.ID, 'zk-comp-1119')
+    botao_definir = (By.CSS_SELECTOR, 'span[id$="comp-187"] [class$="button-cm"]')
+    sala = (By.CSS_SELECTOR, '')
+    hora_inicio = (By.CSS_SELECTOR, 'zk-comp-963')
+    hora_fim = (By.CSS_SELECTOR, 'zk-comp-1119')
     botao_adicionar = (By.CSS_SELECTOR, 'table[id$="comp-159!box"] [class$="button-cm"]')
     botao_salvar = (By.CSS_SELECTOR, 'table[id$="comp-159!box"] [class$="button-cm"]')
+
+    salvar = (By.CSS_SELECTOR, 'table[id$="comp-202!box"] [class$="button-cm"]')
 
     def caminho(self):
         sleep(1)
@@ -48,3 +49,33 @@ class criarAtividade(PageElement):
         self.find_element(self.evento).click()
         sleep(2)
         self.find_element(self.btn_novo).click()
+
+    # --------- Caso de teste: Criação de evento padrão -------------#
+    def ct01_criar_atividade(self, tema, descricao, vagas, duracao, sala, data, hora_inicio, hora_fim):
+
+        try:
+            sleep(1)
+            self.find_element(self.combo_tipo).click()
+            self.find_element(self.tipo).click()
+            self.find_element(self.tema).send_keys(tema)
+            self.find_element(self.descricao).send_keys(descricao)
+            self.find_element(self.vagas).send_keys(vagas)
+            self.find_element(self.duracao).send_keys(duracao)
+            # DATAS DA ATIVIDADE
+            self.find_element(self.botao_definir).click()
+            self.find_element(self.sala).send_keys(sala)
+            self.find_element(self.data).send_keys(data)
+            self.find_element(self.hora_inicio).send_keys(hora_inicio)
+            self.find_element(self.hora_fim).send_keys(hora_fim)
+            self.find_element(self.botao_adicionar).click()
+            self.find_element(self.botao_salvar).click()
+            # RESPONSAVEL
+            self.find_element(self.combo_palestrante).click()
+            self.find_element(self.palestrante).click()
+
+            #self.find_element(self.salvar).click()
+
+            print('\n CT_01 sem erros: o evento foi criado com sucesso')
+
+        except UnexpectedAlertPresentException as e:
+            print("\n [!] CT_01 reportou erro: " + str(e))
--- testes/teste_criar_atividade.py
+++ testes/teste_criar_atividade.py
@@ -18,3 +18,13 @@ criar_atividade = criarAtividade(webdriver)
 
 # --------- Caso de teste: Criação de atividade padrão -------------#
 criar_atividade.caminho()
+criar_atividade.ct01_criar_atividade(
+    tema        = 'ativividade teste',
+    descricao   = 'teste nova atividade',
+    vagas       = '30',
+    duracao     = '8',
+    sala        = '102',
+    data        = '01/01/2021',
+    hora_inicio = '1000',
+    hora_fim    = '1800'
+)
