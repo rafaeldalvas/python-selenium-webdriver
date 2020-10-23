9a6cc23c197216c347041c69c715d5e633c50e0f
João Bonetti <joaopaulodalmeida2@gmail.com>
2020-10-23 01:18:11 -0300

    ct_11 editar_evento

--- pages/page_editar_evento.py
+++ pages/page_editar_evento.py
@@ -1,16 +1,18 @@
+from selenium.common.exceptions import UnexpectedAlertPresentException, NoSuchElementException
 from selenium.webdriver.common.by import By
 from utils.config import PageElement
 from time import sleep
 
 class editarEvento(PageElement):
     # CAMINHO
+    sleep(2)
     calendario = (By.CSS_SELECTOR, "i.fa-calendar")
     admin = (By.CSS_SELECTOR, "a[href$='admEvento/inicial.zul?']")
+    main = (By.CSS_SELECTOR, "a[href$='inicial.zul']")
     cadastro_evento = (By.ID, 'zk-comp-112')
     radio_evento = (By.ID, 'zk-comp-114!real')
     radio_selec_evento = (By.CSS_SELECTOR, 'div.z-list-cell-cnt input')
     btn_editar = (By.CSS_SELECTOR, 'table[id$="zk-comp-134!box"] [class$="z-button-cm"]')
-
     # FORMULARIO PADRAO
     nome = (By.ID, 'zk-comp-148')
     descricao = (By.ID, 'zk-comp-151')
@@ -23,6 +25,7 @@ class editarEvento(PageElement):
     # COMBOBOX TIPO EVENTO
     btn_tipo_evento = (By.ID, 'zk-comp-185!btn')
     tipo_evento = (By.ID, 'zk-comp-214') # Selecionar - Feira
+    select_tipo_evento = (By.ID, 'zk-comp-185!real')
     # FIM COMBOBOX
     inscricao_externa = (By.ID, 'zk-comp-190!real') # Selecionar - Não
     evento_pago_aluno = (By.ID, 'zk-comp-195!real') # Desmarcar o campo "Aluno"
@@ -30,7 +33,7 @@ class editarEvento(PageElement):
 
     # FORMUMARIO RESPONSAVEL
     btn_buscar = (By.CSS_SELECTOR, 'table[id$="comp-159!box"] [class$="button-cm"]')
-    nome_responsavel = (By.ID, 'zk-comp-240')  # Pesquisa - Raquel Alves da Silva
+    nome_responsavel = (By.ID, 'zk-comp-240')  # Pesquisar - Raquel Alves da Silva
     btn_pesquisar_responsavel = (By.CSS_SELECTOR, 'table[id$="comp-247!box"] [class$="button-cm"]')
     checkbox_responsavel = (By.CSS_SELECTOR, '[type="checkbox"]')
     seleciona_nome = (By.ID, 'table[id$="comp-259!box"] [class$="button-cm"]')
@@ -39,18 +42,18 @@ class editarEvento(PageElement):
     # FORMULARIO CERTIFICADO
     btn_editar_certificado = (By.CSS_SELECTOR, 'table[id$="comp-167!box"] [class$="button-cm"]')
     combo_certificado = (By.ID, "zk-comp-233!btn")  # COMBO BOX
-    certificado = (By.CSS_SELECTOR, 'tr[id$="comp-272"] [class="z-combo-item-text"]')  # OPÇÃO PADRAO ICE 2
+    certificado = (By.CSS_SELECTOR, 'tr[id$="zk-comp-277"] [class="z-combo-item-text"]')  # OPÇÃO PADRAO ICE 2
 
     combo_assinatura1 = (By.ID, "zk-comp-238!btn")
-    assinatura1 = (By.CSS_SELECTOR, 'tr[id$="comp-301"] [class="z-combo-item-text"]')
+    assinatura1 = (By.CSS_SELECTOR, 'tr[id$="zk-comp-293"] [class="z-combo-item-text"]')
     funcao1 = (By.ID, "zk-comp-242")
 
     combo_assinatura2 = (By.ID, "zk-comp-247!btn")
-    assinatura2 = (By.CSS_SELECTOR, 'tr[id$="comp-325"] [class="z-combo-item-text"]')
+    assinatura2 = (By.CSS_SELECTOR, 'tr[id$="zk-comp-332"] [class="z-combo-item-text"]')
     funcao2 = (By.ID, "zk-comp-251")
 
     combo_assinatura3 = (By.ID, "zk-comp-256!btn")
-    assinatura3 = (By.CSS_SELECTOR, 'tr[id$="comp-382"] [class="z-combo-item-text"]')
+    assinatura3 = (By.CSS_SELECTOR, 'tr[id$="zk-comp-387"] [class="z-combo-item-text"]')
     funcao3 = (By.ID, "zk-comp-260")
 
     btn_salvar = (By.CSS_SELECTOR, 'table[id$="comp-263!box"] [class$="button-cm"]')
@@ -61,11 +64,28 @@ class editarEvento(PageElement):
 
     # EXCLUIR EVENTO
     btn_excluir = (By.CSS_SELECTOR, 'table[id$="zk-comp-136!box"] [class$="z-button-cm"]')
+    # confirm_excluir = (By.CSS_SELECTOR, 'table[id$="zk-comp-229!box"] [class$="z-button-cm"]') #ESSE É O VERDADEIRO, CRIEI O DE BAIXO PRA N FICAR EXCLUINDO NOSSOS EVENTOS *******MUDAR DEPOIS*******
+    confirm_excluir = (By.CSS_SELECTOR, 'table[id$="zk-comp-230!box"] [class$="z-button-cm"]')
 
     # CANCELAR
     btn_cancelar = (By.CSS_SELECTOR, 'table[id$="comp-201!box"] [class$="button-cm"]')
 
+    # JANELA DE AVISO
+    alert_tipo = (By.CSS_SELECTOR, 'div[class$="modal-cm-noborder"]')
+    click_ok = (By.CSS_SELECTOR, 'table[id$="zk-comp-229!box"] [class$="z-button-cm"]')
 
+
+
+    def espera_mensagem(self):
+        try:
+            self.find_element(self.alert_tipo)
+            return True
+        except NoSuchElementException:
+            return False
+
+    def clicarMain(self):
+        sleep(1)
+        self.find_element(self.main).click()
 
 
     def caminho(self, ct_08 = False, ct_10 = False):
@@ -80,34 +100,35 @@ class editarEvento(PageElement):
         sleep(1)
         if ct_10 is False:
             self.find_element(self.radio_selec_evento).click()
-        sleep(2)
+        sleep(1)
         if ct_08 is False:
             self.find_element(self.btn_editar).click()
 
     def preenche_certificado(self, funcao1, funcao2, funcao3):
         self.find_element(self.btn_editar_certificado).click()
-        sleep(2)
+        sleep(1)
         self.find_element(self.combo_certificado).click()
         self.find_element(self.certificado).click()
         self.find_element(self.combo_assinatura1).click()
         self.find_element(self.assinatura1).click()
+        self.find_element(self.funcao1).clear()
         self.find_element(self.funcao1).send_keys(funcao1)
         self.find_element(self.combo_assinatura2).click()
         self.find_element(self.assinatura2).click()
+        self.find_element(self.funcao2).clear()
         self.find_element(self.funcao2).send_keys(funcao2)
         self.find_element(self.combo_assinatura3).click()
         self.find_element(self.assinatura3).click()
+        self.find_element(self.funcao3).clear()
         self.find_element(self.funcao3).send_keys(funcao3)
         self.find_element(self.btn_salvar).click()
-        sleep(2)
+        sleep(1)
         self.find_element(self.btn_ok).click()
 
     def preenche_responsavel(self, nome_responsavel):
         sleep(1)
         self.find_element(self.btn_buscar).click()
         sleep(1)
-        # self.sendKeys(Keys.RETURN)
-        # sleep(1)
         self.find_element(self.nome_responsavel).send_keys(nome_responsavel)
         self.find_element(self.btn_pesquisar_responsavel).click()
         self.find_element(self.checkbox_responsavel).click()
@@ -117,55 +138,149 @@ class editarEvento(PageElement):
     # --------- Caso de teste: Edição de evento padrão -------------#
     def ct_07_editar_evento(self,nome, descricao, site, email_responsavel, inicio_evento, fim_evento, inicio_inscricao,
                           fim_inscricao, funcao1, funcao2, funcao3, nome_responsavel):
-        sleep(1)
-        self.find_element(self.nome).send_keys(nome)
-        self.find_element(self.descricao).send_keys(descricao)
-        self.find_element(self.site).send_keys(site)
-        self.find_element(self.email_responsavel).send_keys(email_responsavel)
 
-        # CERTIFICADO
-        self.preenche_certificado(funcao1, funcao2, funcao3)
-        # FIM CERTIFICADO
+        try:
+            sleep(1)
+            self.find_element(self.nome).send_keys(nome)
+            self.find_element(self.descricao).send_keys(descricao)
+            self.find_element(self.site).send_keys(site)
+            self.find_element(self.email_responsavel).send_keys(email_responsavel)
 
-        # RESPONSAVEL
-        self.preenche_responsavel(nome_responsavel) # DA ERRO DE JS AQUI
-        # FIM RESPONSAVEL
+            # CERTIFICADO
+            self.preenche_certificado(funcao1, funcao2, funcao3)
+            # FIM CERTIFICADO
 
-        self.find_element(self.inicio_evento).send_keys(inicio_evento)
-        self.find_element(self.fim_evento).send_keys(fim_evento)
-        self.find_element(self.inicio_inscricao).send_keys(inicio_inscricao)
-        self.find_element(self.fim_inscricao).send_keys(fim_inscricao)
+            # RESPONSAVEL
+            self.preenche_responsavel(nome_responsavel) # DA ERRO DE JS AQUI
+            # FIM RESPONSAVEL
 
-        # TIPO DE EVENTO
-        self.find_element(self.btn_tipo_evento).click()
-        self.find_element(self.tipo_evento).click()
-        # FIM TIPO DE EVENTO
+            self.find_element(self.inicio_evento).send_keys(inicio_evento)
+            self.find_element(self.fim_evento).send_keys(fim_evento)
+            self.find_element(self.inicio_inscricao).send_keys(inicio_inscricao)
+            self.find_element(self.fim_inscricao).send_keys(fim_inscricao)
 
-        self.find_element(self.inscricao_externa).click()
-        self.find_element(self.evento_pago_aluno).click()
-        self.find_element(self.evento_pago_externo).click()
+            # TIPO DE EVENTO
+            self.find_element(self.btn_tipo_evento).click()
+            self.find_element(self.tipo_evento).click()
+            # FIM TIPO DE EVENTO
 
-        #self.find_element(self.btn_enviar).click()
-        sleep(1)
+            self.find_element(self.inscricao_externa).click()
+            self.find_element(self.evento_pago_aluno).click()
+            self.find_element(self.evento_pago_externo).click()
 
+            #self.find_element(self.btn_enviar).click()
+            print('\n CT_07 sem erros: o evento foi editado com sucesso')
+
+        except UnexpectedAlertPresentException as e:
+            print("\n [!] CT_07 reportou erro: " + str(e))
 
 # --------- Caso de teste: Exclusão de eventos -------------#
     def ct_08_editar_evento(self):
-        self.find_element(self.btn_excluir).click()
+        try:
+            self.find_element(self.btn_excluir).click()
+            sleep(1)
+            self.find_element(self.confirm_excluir).click()
+            print('\n CT_08 sem erros: o evento foi excluído com sucesso')
+
+        except UnexpectedAlertPresentException as e:
+            print("\n [!] CT_08 reportou erro: " + str(e))
 
 # --------- Caso de teste: Cancelar transação -------------#
-    def ct_09_editar_evento(self, nome, descricao):
-        sleep(1)
-        self.find_element(self.nome).send_keys(nome)
-        self.find_element(self.descricao).send_keys(descricao)
-        self.find_element(self.btn_cancelar).click()
+    def ct_09_editar_evento(self):
+        try:
+            sleep(1)
+            self.find_element(self.btn_cancelar).click()
+            print('\n CT_09 sem erros: A operação foi cancelada com sucesso')
+
+        except UnexpectedAlertPresentException as e:
+            print("\n [!] CT_09 reportou erro: " + str(e))
 
 # -- Caso de teste: Nenhum evento selecionado ao editar ---#
-    def ct_10_editar_evento(self, ct_10):
-        self.caminho(False, ct_10)
+    def ct_10_editar_evento(self):
+        try:
+            sleep(1)
+            msg = self.espera_mensagem()
+            if msg is True:
+                print("\n CT_10 reportou erro: O sistema pediu para selecionar um evento")
+                sleep(1)
+                self.find_element(self.click_ok).click()
+            else:
+                 print("\n [!] CT_10 reportou erro: Houve acesso ao formulário sem selecionar um evento")
+        except UnexpectedAlertPresentException as e:
+            print("\n [!] CT_09 reportou erro: " + str(e))
+
 
 # -- Caso de teste: Campos obrigatórios não preenchidos ---#
-    def ct_11_editar_evento(self):
-        self.find_element(self.nome).send_keys('')
-        self.find_element(self.descricao).send_keys('')
+    def ct_11_editar_evento(self, nome, descricao, inicio_evento, fim_evento, inicio_inscricao, fim_inscricao, nome_responsavel):
+        erro = True
+        sleep(1)
+        try:
+            self.find_element(self.nome).clear()
+            self.find_element(self.btn_enviar).click()
+            msg = self.espera_mensagem()
+            if msg is True:
+                erro = False
+
+            if erro is False:
+                self.find_element(self.nome).send_keys(nome)
+                self.find_element(self.descricao).clear()
+                self.find_element(self.btn_enviar).click()
+                msg = self.espera_mensagem()
+                if msg is True:
+                    erro = False
+
+            if erro is False:
+                self.find_element(self.descricao).send_keys(descricao)
+                self.find_element(self.inicio_evento).clear()
+                self.find_element(self.btn_enviar).click()
+                msg = self.espera_mensagem()
+                if msg is True:
+                    erro = False
+
+            if erro is False:
+                self.find_element(self.inicio_evento).send_keys(inicio_evento)
+                self.find_element(self.fim_evento).clear()
+                self.find_element(self.btn_enviar).click()
+                msg = self.espera_mensagem()
+                if msg is True:
+                    erro = False
+
+            if erro is False:
+                self.find_element(self.fim_evento).send_keys(fim_evento)
+                self.find_element(self.inicio_inscricao).clear()
+                self.find_element(self.btn_enviar).click()
+                msg = self.espera_mensagem()
+                if msg is True:
+                    erro = False
+
+            if erro is False:
+                self.find_element(self.inicio_inscricao).send_keys(inicio_inscricao)
+                self.find_element(self.fim_inscricao).clear()
+                self.find_element(self.btn_enviar).click()
+                msg = self.espera_mensagem()
+                if msg is True:
+                    erro = False
+
+            if erro is False:
+                self.find_element(self.fim_inscricao).send_keys(fim_inscricao)
+                self.find_element(self.select_tipo_evento).clear()
+                self.find_element(self.btn_enviar).click()
+                msg = self.espera_mensagem()
+                if msg is True:
+                    erro = False
+
+            if erro is False:
+                self.preenche_responsavel(nome_responsavel)
+
+            if erro is False:
+                print("\n CT_11 reportou erro: O sistema exibiu os campos faltantes")
+            else:
+                print("\n [!] CT_11 reportou erro: Evento editado com campos obrigatorios nao preenchidos")
+
+        except UnexpectedAlertPresentException as e:
+            print("\n CT_04 reportou erro: " + str(e))
+
+
+
+
         #self.find_element(self.btn_enviar).click()
--- testes/teste_editar_evento.py
+++ testes/teste_editar_evento.py
@@ -16,37 +16,47 @@ login_professor.realiza_login(
 
 editar_evento = editarEvento(webdriver)
 
-# --------- Caso de teste: Edição de evento padrão -------------#
+# # --------- Caso de teste: Edição de evento padrão -------------#
+# editar_evento.caminho()
+# editar_evento.ct_07_editar_evento(
+#     nome                = 'Evento teste ct07 Alt',
+#     descricao           = 'Alterando evento',
+#     site                = 'alterei.com.br',
+#     email_responsavel   = 'alterado@teste.com',
+#     inicio_evento       = '02/02/2021',
+#     fim_evento          = '16/02/2021',
+#     inicio_inscricao    = '02/01/2021',
+#     fim_inscricao       = '01/02/2021',
+#     funcao1             = 'Professor Alterado',
+#     funcao2             = 'Gerente Alterado',
+#     funcao3             = 'Diretor Alterado',
+#     nome_responsavel    = 'Raquel Alves da Silva'
+# )
+#
+# # --------- Caso de teste: Exclusão de evento -------------#
+# editar_evento.clicarMain()
+# editar_evento.caminho(True)
+# editar_evento.ct_08_editar_evento()
+#
+# # --------- Caso de teste: Cancelar transação -------------#
+# editar_evento.clicarMain()
+# editar_evento.caminho()
+# editar_evento.ct_09_editar_evento()
+#
+# # --------- Caso de teste: Nenhum evento selecionado ao editar -------------#
+# editar_evento.clicarMain()
+# editar_evento.caminho(False, True)
+# editar_evento.ct_10_editar_evento()
+#
+# # --------- Caso de teste: Campos obrigatórios não preenchidos -------------#
+# editar_evento.clicarMain()
 editar_evento.caminho()
-editar_evento.ct_07_editar_evento(
+editar_evento.ct_11_editar_evento(
     nome                = 'Evento teste ct07 Alt',
     descricao           = 'Alterando evento',
-    site                = 'alterei.com.br',
-    email_responsavel   = 'alterado@teste.com',
     inicio_evento       = '02/02/2021',
     fim_evento          = '16/02/2021',
     inicio_inscricao    = '02/01/2021',
     fim_inscricao       = '01/02/2021',
-    funcao1             = 'Professor Alterado',
-    funcao2             = 'Gerente Alterado',
-    funcao3             = 'Diretor Alterado',
     nome_responsavel    = 'Raquel Alves da Silva'
 )
-
-# --------- Caso de teste: Exclusão de evento -------------#
-editar_evento.caminho(True)
-editar_evento.ct_08_editar_evento()
-
-# --------- Caso de teste: Cancelar transação -------------#
-editar_evento.caminho()
-editar_evento.ct_09_editar_evento(
-    nome      = 'Evento cancelando transação',
-    descricao = 'Cancelando transação'
-)
-
-# --------- Caso de teste: Nenhum evento selecionado ao editar -------------#
-editar_evento.ct_10_editar_evento(True)
-
-# --------- Caso de teste: Campos obrigatórios não preenchidos -------------#
-editar_evento.caminho()
-editar_evento.ct_11_editar_evento()
