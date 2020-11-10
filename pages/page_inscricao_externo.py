7c6093e527818329f323628e1a0846fb44e35126
Rafael Dalvas <rafaeldalvas@gmail.com>
2020-11-10 19:12:37 -0300

    ct50 inscricao_externo

--- pages/page_inscricao_externo.py
+++ pages/page_inscricao_externo.py
@@ -7,21 +7,88 @@ from selenium.webdriver.support.wait import WebDriverWait
 from utils.config import PageElement
 from time import sleep
 
+
 class inscricaoExterno(PageElement):
-    #CAMINHO
+    # CAMINHO
     inscricao_eventos = (By.CSS_SELECTOR, "a[href$='listaEventos.zul']")
-    evento = (By.CSS_SELECTOR, 'table[id$="zk-comp-99!box"] [class$="button-cm"]')
+    evento = (By.CSS_SELECTOR, 'table[id$="comp-99!box"] [class$="button-cm"]')
+
+    proxima_pagina = (By.CLASS_NAME, 'z-paging-next')
+    atividade = (By.XPATH, '/html/body/div/div/div/div/div/div/div/div['
+                           '2]/div/section/div/div/section/div/div/section/div/div[1]/div[2]/div[1]/div/div/div/div['
+                           '3]/div[2]/table/tbody[2]/tr[5]/td[3]/div/table/tbody/tr/td/span/table/tbody/tr[2]/td[2]')
+    # FORMULARIO-1
+    cpf = (By.ID, 'zk-comp-45')
+    nome = (By.ID, 'zk-comp-48')
+    email = (By.ID, 'zk-comp-51')
+    confirmar = (By.ID, 'zk-comp-54')
+    # FORMULARIO-2
+    telefone = (By.ID, 'zk-comp-97')
+    celular = (By.ID, 'zk-comp-100')
+    concluir = (By.CSS_SELECTOR, 'table[id$="comp-102!box"] [class$="button-cm"]')
+
+    alert_tipo = (By.CSS_SELECTOR, 'div[class="z-separator-hor-bar"]')
+    alert_texto = (By.XPATH, '//*/div[2]/div[1]/div/div/div/div/div[2]/div/table[1]/tbody/tr/td[3]/div/span')
+    btn_ok_alert = (By.XPATH, '//*/div[2]/div[1]/div/div/div/div/div[2]/div/table[2]/tbody/tr/td/span/table/tbody/tr['
+                              '2]/td[2]')
+
+    def caminho(self):
+        sleep(1)
+        self.find_element(self.inscricao_eventos).click()
+        sleep(1)
+        self.find_element(self.evento).click()
+        sleep(1)
+
+    def espera_mensagem(self):
+        try:
+            alert_tipo = WebDriverWait(self.webdriver, poll_frequency=0.2, timeout=5)
+            alert_tipo.until(expected_conditions.visibility_of_element_located(self.alert_tipo))
+            if alert_tipo is not None:
+                return True
+        except TimeoutException:
+            return False
 
     # ---------- Casos de teste: Evento não selecionado --------------#
-    #def ct50_inscricao_externo(self):
+    def ct50_inscricao_externo(self, cpf, nome, email, telefone, celular):
+        txt = "Inscrição solicitada!"
+        try:
+            self.find_element(self.proxima_pagina).click()
+            sleep(1)
+            self.find_element(self.atividade).click()
+            sleep(1)
+            self.find_element(self.cpf).send_keys(cpf)
+            self.find_element(self.nome).send_keys(nome)
+            self.find_element(self.email).send_keys(email)
+            self.find_element(self.confirmar).click()
+            sleep(1)
+            self.find_element(self.telefone).send_keys(telefone)
+            self.find_element(self.celular).send_keys(celular)
+            self.find_element(self.concluir).click()
+            msg = self.espera_mensagem()
+            if msg is True:
+                if txt in self.find_element(self.alert_texto).text:
+                    print("\n CT_50 sem erros: " + self.find_element(self.alert_texto).text)
+                else:
+                    print("\n [!] CT_50 reportou erro: " + self.find_element(self.alert_texto).text)
+                self.find_element(self.btn_ok_alert).click()
+            else:
+                print("\n CT_50 sem erros: Inscrição solicitada com sucesso!")
+
+        except UnexpectedAlertPresentException as e:
+            print("\n [!] CT_50 reportou erro: " + str(e))
+
+        except ElementClickInterceptedException:
+            print("\n [!] CT_22 reportou erro: " + self.find_element(self.alert_texto).text)
+            self.find_element(self.btn_ok_alert).click()
+
     # - Casos de teste: Confirmar inscrição de usuário externo padrão-#
-    #def ct51_inscricao_externo(self):
+    # def ct51_inscricao_externo(self):
     # ---------- Casos de teste: Prazo para inscrição vencido --------#
-    #def ct52_inscricao_externo(self):
+    # def ct52_inscricao_externo(self):
     # --------------- Casos de teste: CPF inválido -------------------#
-    #def ct53_inscricao_externo(self):
+    # def ct53_inscricao_externo(self):
     # -------------- Casos de teste: Email inválido ------------------#
-    #def ct54_inscricao_externo(self):
+    # def ct54_inscricao_externo(self):
     # - Casos de teste: Campos obrigatórios no primeiro formulário ---#
-    #def ct55_inscricao_externo(self):
+    # def ct55_inscricao_externo(self):
     # -- Casos de teste: Campos obrigatórios no segundo formulário ---#
--- testes/teste_inscricao_externo.py
+++ testes/teste_inscricao_externo.py
@@ -4,9 +4,20 @@ from pages.page_inscricao_externo import inscricaoExterno
 webdriver = webdriver.Chrome(r'd:\Downloads\chromedriver_win32\chromedriver.exe')
 
 url = "https://integra-h.nrc.ice.ufjf.br/integra/geral/menuEvento.zul"
-webdriver.get(url)
+
+inscricao_externo = inscricaoExterno(webdriver)
 
 # ---------- Casos de teste: Evento não selecionado --------------#
+webdriver.get(url)
+inscricao_externo.caminho()
+inscricao_externo.ct50_inscricao_externo(
+    cpf = '96055817012',
+    nome = 'Participante Externo Teste',
+    email = 'pexterno@teste.com',
+    telefone = '43663746970',
+    celular = '51405831810'
+)
+
 # - Casos de teste: Confirmar inscrição de usuário externo padrão-#
 # ---------- Casos de teste: Prazo para inscrição vencido --------#
 # --------------- Casos de teste: CPF inválido -------------------#
